b2430fe3ae18f3357a7d7e7d23fabaa161857e86
ilhambudiprasetyo <ilham.budi@mekari.com>
2022-07-21 06:27:39 +0700

    fix settings fragment & add layout

--- app/src/main/java/com/app/msm/ui/main/setting/SettingFragment.kt
+++ app/src/main/java/com/app/msm/ui/main/setting/SettingFragment.kt
@@ -3,10 +3,11 @@ package com.app.msm.ui.main.setting
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.app.msm.R
 import com.app.msm.databinding.FragmentSettingBinding
 import com.app.msm.helper.viewBinding
 
-class SettingFragment : Fragment() {
+class SettingFragment : Fragment(R.layout.fragment_setting) {
 
     private val binding by viewBinding(FragmentSettingBinding::bind)
 
