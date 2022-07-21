0ff705bc78faf3c54826d9ee583b43a8d56ec4f6
ilhambudiprasetyo <ilham.budi@mekari.com>
2022-07-21 07:12:37 +0700

    add pref key

--- app/src/main/java/com/app/msm/data/preferences/SharedPrefKey.kt
+++ app/src/main/java/com/app/msm/data/preferences/SharedPrefKey.kt
@@ -1,5 +1,6 @@
 package com.app.msm.data.preferences
 
 object SharedPrefKey {
-    const val IS_LOGIN = "is_login"
+    const val SESSION_IS_LOGIN = "session_is_login"
+    const val SESSION_EMAIL = "session_email"
 }
